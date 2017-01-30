61caa045559c5f59f2261816987b9eb6342a26c4
Diego Suarez and Tyler Ferrin <diego+tyler@>
2017-01-30 15:33:15 -0800

    autocomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { MapPage } from '../pages/map/map';
+import { PageGmapAutocomplete } from '../pages/page-gmap-autocomplete/page-gmap-autocomplete';
+import { ModalAutocompleteItems } from  '../pages/modal-autocomplete-items/modal-autocomplete-items';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    MapPage
+    MapPage,
+    PageGmapAutocomplete,
+    ModalAutocompleteItems
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -17,7 +21,9 @@ import { MapPage } from '../pages/map/map';
   entryComponents: [
     MyApp,
     HomePage,
-    MapPage
+    MapPage,
+    PageGmapAutocomplete,
+    ModalAutocompleteItems
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController} from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
 import { MapPage } from '../map/map';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PageGmapAutocomplete } from '../page-gmap-autocomplete/page-gmap-autocomplete';
+import { ModalAutocompleteItems } from '../modal-autocomplete-items/modal-autocomplete-items';
+
 declare var google;
 
 @Component({
@@ -10,10 +13,17 @@ declare var google;
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  address:any = {
+      place: '',
+      set: false,
+  };
+  placesService:any;
+  map: any;
+  @ViewChild('map') mapElement: ElementRef;
+  placedetails: any;
   destinationForm: FormGroup;
   currentLocationToggle: boolean = true;;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+  constructor(private modalCtrl: ModalController, public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
     this.destinationForm = formBuilder.group({
        destination: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z0-9]*')])],
    });
@@ -22,17 +32,108 @@ export class HomePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
+    this.initPlacedetails();
+    this.loadMap();
+  }
+  loadMap(){
+
+    Geolocation.getCurrentPosition().then((position) => {
+
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    }, (err) => {
+      console.log(err);
+    });
+
+
   }
 
+  showModal() {
+      this.reset();
+      let modal = this.modalCtrl.create(ModalAutocompleteItems);
+      modal.onDidDismiss(data => {
+          console.log('page > modal dismissed > data > ', data);
+          if(data){
+              this.address.place = data.description;
+              // get details
+              this.getPlaceDetail(data.place_id);
+          }
+      })
+      modal.present();
+  }
+  private reset() {
+      // this.initPlacedetails();
+      this.address.place = '';
+      this.address.set = false;
+  }
+  private getPlaceDetail(place_id:string):void {
+      var self = this;
+      var request = {
+          placeId: place_id
+      };
+       this.placesService = new google.maps.places.PlacesService(this.map);
+        this.placesService.getDetails(request, callback)
+      function callback(place, status) {
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+              // set full address
+              self.placedetails.address = place.formatted_address;
+              self.placedetails.lat = place.geometry.location.lat();
+              self.placedetails.lng = place.geometry.location.lng();
+              for (var i = 0; i < place.address_components.length; i++) {
+                  let addressType = place.address_components[i].types[0];
+                  let values = {
+                      short_name: place.address_components[i]['short_name'],
+                      long_name: place.address_components[i]['long_name']
+                  }
+                  if(self.placedetails.components[addressType]) {
+                      self.placedetails.components[addressType].set = true;
+                      self.placedetails.components[addressType].short = place.address_components[i]['short_name'];
+                      self.placedetails.components[addressType].long = place.address_components[i]['long_name'];
+                  }
+              }
+              // populate
+              // self.address.set = true;
+              console.log('page > getPlaceDetail > details > ', self.placedetails);
+          }else{
+              console.log('page > getPlaceDetail > status > ', status);
+          }
+      }
+  }
+  private initPlacedetails() {
+      this.placedetails = {
+          address: '',
+          lat: '',
+          lng: '',
+          components: {
+              route: { set: false, short:'', long:'' },                           // calle
+              street_number: { set: false, short:'', long:'' },                   // numero
+              sublocality_level_1: { set: false, short:'', long:'' },             // barrio
+              locality: { set: false, short:'', long:'' },                        // localidad, ciudad
+              administrative_area_level_2: { set: false, short:'', long:'' },     // zona/comuna/partido
+              administrative_area_level_1: { set: false, short:'', long:'' },     // estado/provincia
+              country: { set: false, short:'', long:'' },                         // pais
+              postal_code: { set: false, short:'', long:'' },                     // codigo postal
+              postal_code_suffix: { set: false, short:'', long:'' },              // codigo postal - sufijo
+          }
+      };
+  }
   goToMap(){
     this.navCtrl.push(MapPage);
   }
 
+  test(){
+    this.navCtrl.push(PageGmapAutocomplete);
+  }
+
   getRoutes($event) {
-    if (!this.destinationForm.valid){
-      alert("you fucked up!")
-    }else{
       this.navCtrl.push(MapPage, this.destinationForm.value.destination);
-    }
   }
 }
